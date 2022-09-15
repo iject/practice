65db15c8d1bf41f169190b12bc56810dc51bcd92
iject <91547956+iject@users.noreply.github.com>
2022-09-15 11:42:21 +0300

    Add files via upload

---
+++ 15.09.22/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._09._22
+{
+    class Complex
+    {
+        public double re;
+        public double im;
+
+        public Complex(double re, double im)
+        {
+            this.re = re;
+            this.im = im;
+        }
+        public Complex()
+        {
+            this.re = 0;
+            this.im = 0;
+        }
+
+        public double Re { get; set; }
+        public double Im { get => im; set => im = value; }
+
+        static public Complex add(Complex A, Complex B)
+        {
+            return new Complex(A.re + B.re, A.im + B.im);
+        }
+
+        public void showAlg()
+        {
+            Console.WriteLine("{0} + {1}i", this.re, this.im);
+        }
+
+        public double abs()
+        {
+            return Math.Sqrt(this.re * this.re + this.im * this.im);
+        }
+        public double arg()
+        {
+            return Math.Atan(this.im / this.re);
+        }
+        public void showGeom()
+        {
+            Console.WriteLine("{0}(*cos{1} + i * sin{1} + {1}i)", abs(), arg());
+        }
+      
+    }
+}
---
+++ 15.09.22/Program.cs
@@ -0,0 +1,9 @@
+﻿using _15._09._22;
+
+Complex z1 = new Complex(1, 1);
+Complex z2 = new Complex(1, 1);
+Complex z3 = new Complex();
+z3 = Complex.add(z1, z2);
+
+z3.showAlg();
+Console.WriteLine(z3.re);
