da2cfe98887574094b0596d78907f8b4a8b5cb35
iject <91547956+iject@users.noreply.github.com>
2022-09-22 11:41:01 +0300

    Add files via upload

---
+++ 22.09.22/22.09.22/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _22._09._22
+{
+    internal class tocki
+    {
+        private double x;
+        private double y;
+        private double z;
+
+        public double X { get; set;}
+        public double Y { get; set;}
+        public double Z { get; set;}
+
+        public tocki()
+        {
+            this.x = 0;
+            this.y = 0;
+            this.z = 0;
+        }
+
+        public tocki(double x,double y,double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static double dist(tocki A, tocki B)
+        {
+            return Math.Sqrt(Math.Pow(A.x - B.x, 2) + Math.Pow(A.y - B.y, 2));
+        }
+
+        public double dist_to_zero (tocki A)
+        {
+            return dist(A, new tocki(0, 0, 0));
+        }
+
+        public vector(tocki A, tocki B)
+        {
+
+        }
+    }
+}
---
+++ 22.09.22/22.09.22/Class2.cs
@@ -0,0 +1,5 @@
+﻿using _22._09._22;
+
+tocki p1 = new tocki(1, 2, 3);
+tocki p2 = new tocki(1, 2, 3);
+Console.WriteLine(tocki.dist(p1, p2));
